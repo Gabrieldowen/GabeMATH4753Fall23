772e0385e451d68e3ae3eefb3f3acd4fc74ab527
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-10-25 19:06:04 -0500

    Lab 7 and 8 changes

---
+++ R/GabesMyclt.R
@@ -0,0 +1,16 @@
+#' Lab 8 function
+#'
+#' @param n A sample size
+#' @param iter number of iterations description
+#' @return a histogram and vector of sums
+#' @export
+#'
+#' @examples
+#' GabesMyclt(n=10, iter=10000)
+GabesMyclt<-function(n,iter){
+  y=runif(n*iter,0,5) # A
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE) #B
+  sm=apply(data,2,sum) #C
+  hist(sm)
+  sm
+}
